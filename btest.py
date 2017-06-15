fff5055a2566abaa8b1e034a1696a6548d41bf56
vdhert <tymoteusz.oleniecki@gmail.com>
2017-06-15 03:22:52 +0200

    SC rebuilding function for dbg in utils

--- btest.py
+++ btest.py
@@ -2,6 +2,8 @@ import pickle
 from cabsDock.cmap import ContactMapFactory
 from cabsDock.cmap import ContactMap
 
+from cabsDock.utils import rebuildCb
+
 with open('test_traj.pck') as f:
     trajectory = pickle.load(f)
 with open('test_complex.pck') as f:
@@ -13,10 +15,12 @@ tra = trajectory.filter(1000)
 lig = tra.select('chain B')
 
 for lig in initial_complex.ligand_chains:
-   cmf = ContactMapFactory(initial_complex.receptor_chains, lig, trajectory.template)      #where from comes rec ch name?
-   cmaps = cmf.mk_cmap(trajectory.coordinates, 6.5)
-   for n, cmap in enumerate(cmaps):
-       with open('btest_res/cm_%s_%i.txt' % (lig, n), 'w') as f:
-          cmap.save_txt(f)
-       cmap.save_png('btest_res/cm_%s_%i' % (lig, n))
+    cmf = ContactMapFactory(initial_complex.receptor_chains, lig, trajectory.template)      #where from comes rec ch name?
+    rebuildCb(trajectory.coordinates[0, 0, cmf.inds1,], cmf.ats1)
+    #~ cmaps = cmf.mk_cmap(trajectory.coordinates, 6.5)
+    #~ for n, cmap in enumerate(cmaps):
+        #~ with open('res_btest/cm_%s_%i.txt' % (lig, n), 'w') as f:
+            #~ cmap.save_txt(f)
+        #~ cmap.save_png('res_btest/cm_%s_%i' % (lig, n))
+    #~ import pdb; pdb.set_trace()
 
--- cabsDock/utils.py
+++ cabsDock/utils.py
@@ -9,6 +9,28 @@ from pkg_resources import resource_filename
 # Dictionary for conversion of secondary structure from DSSP to CABS
 CABS_SS = {'C': 1, 'H': 2, 'T': 3, 'E': 4, 'c': 1, 'h': 2, 't': 3, 'e': 4}
 
+#sidechains relative coords
+SIDECNT = { 'CYS': (-0.139, -1.265, 1.619, 0.019, -0.813, 1.897),
+            'GLN': (-0.095, -1.674, 2.612, 0.047, -0.886, 2.991),
+            'ILE': (0.094, -1.416, 1.836, -0.105, -0.659, 2.219),
+            'SER': (0.121, -1.476, 1.186, 0.223, -1.042, 1.571),
+            'VAL': (0.264, -1.194, 1.531, 0.077, -0.631, 1.854),
+            'MET': (-0.04, -1.446, 2.587, 0.072, -0.81, 2.883),
+            'PRO': (-0.751, -1.643, 0.467, -1.016, -1.228, 0.977),
+            'LYS': (0.032, -1.835, 2.989, 0.002, -0.882, 3.405),
+            'THR': (0.075, -1.341, 1.398, 0.051, -0.909, 1.712),
+            'PHE': (0.151, -1.256, 3.161, -0.448, -0.791, 3.286),
+            'ALA': (0.253, -1.133, 0.985, 0.119, -0.763, 1.312),
+            'HIS': (-0.301, -1.405, 2.801, -0.207, -0.879, 3.019),
+            'GLY': (0.0, -0.111, -0.111, 0.0, -0.111, -0.111),
+            'ASP': (-0.287, -1.451, 1.989, 0.396, -0.798, 2.313),
+            'GLU': (-0.028, -1.774, 2.546, 0.096, -0.923, 3.016),
+            'LEU': (-0.069, -1.247, 2.292, 0.002, -0.462, 2.579),
+            'ARG': (-0.057, -2.522, 3.639, -0.057, -1.21, 3.986),
+            'TRP': (0.558, -1.694, 3.433, -0.06, -0.574, 3.834),
+            'ASN': (-0.402, -1.237, 2.111, 0.132, -0.863, 2.328),
+            'TYR': (0.308, -1.387, 3.492, -0.618, -0.799, 3.634)}
+
 # Library of 1000 random peptides with up to 50 amino acids each
 RANDOM_LIGAND_LIBRARY = np.reshape(
     np.fromfile(resource_filename('cabsDock', 'data/data2.dat'), sep=' '), (1000, 50, 3)
@@ -198,3 +220,45 @@ def kmedoids(D, k, tmax=100):
 
     # return results
     return M, C
+
+def rebuildCb(vec, nms):
+    """Takes vector of C alpha coords and residue names and returns vector of C beta coords."""
+    vec = np.insert(vec, 0, vec[0] - (vec[2] - vec[1]), axis=0)
+    vec = np.append(vec, np.array([vec[-1] + (vec[-2] - vec[-3])]), axis=0)
+
+    nvec = np.zeros((len(vec) - 2, 3))
+    for i in xrange(len(vec) - 2):
+        c1, c2, c3 = vec[i: i + 3]
+
+        rdif = c3 - c1
+        r2_1 = c1 - c2
+        r2_3 = c3 - c2
+        rsum = r2_3 + r2_1
+        z = -1 * rsum / np.linalg.norm(rsum)
+        x = rdif / np.linalg.norm(rdif)
+        y = np.cross(x, z)
+
+        w = np.cross(z, np.array([0, 0, 1]))
+
+        ph_t = np.array([1, 0, 0]), w
+        cph = np.dot(*ph_t)
+        sph = np.linalg.norm(np.cross(*ph_t))
+
+        cps = np.dot(w, x)
+        sps = np.linalg.norm(np.cross(w, x))
+
+        th_t = (np.array([0, 0, 1]), z)
+        cth = np.dot(*th_t)
+        sth = np.linalg.norm(np.cross(*th_t))
+
+        rot = np.matrix( [  [cps * cph - sps * cth * sph, cps * sph + sph * cth * cph, sps * sth],
+                            [-sps * cph - cps * cth * sph, -sps * sph + cps * cth * cph, cps * sth],
+                            [sth * sph, -sth * cph, cth]])
+
+        cb = np.dot(np.array(SIDECNT['ALA'][:3]), rot)
+
+        nvec[i] = cb + c2
+
+    import imp
+    pdbx = imp.load_source('pdbx', '/usr/lib/python2.7/pdb.py')
+    pdbx.set_trace()
