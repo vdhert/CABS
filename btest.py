3ea15567ec924c76ff085e92e5737f9eb6232a41
vdhert <tymoteusz.oleniecki@gmail.com>
2017-06-20 05:08:16 +0200

    Testfile upgraded.

--- btest.py
+++ btest.py
@@ -1,36 +1,18 @@
 import pickle
-from cabsDock.cmap import ContactMapFactory
-from cabsDock.cmap import ContactMap
-
-from cabsDock.utils import SCModeler
+from cabsDock.job import Job
+from cabsDock.protein import ProteinComplex
 
 try:
     from pymol import cmd
 except: pass
 
-with open('test_traj.pck') as f:
-    trajectory = pickle.load(f)
-with open('test_complex.pck') as f:
-    initial_complex = pickle.load(f)
-with open('test_clusters.pck') as f:
-    C = pickle.load(f)
+with open('traj.pck') as f:
+    tr = pickle.load(f)
+with open('flti.pck') as f:
+    flt = pickle.load(f)
+with open('clst.pck') as f:
+    c = pickle.load(f)
 
-tra = trajectory.filter(1000)
-lig = tra.select('chain B')
-scm = SCModeler(trajectory.template)
-
-for lig in initial_complex.ligand_chains:
-    cmf = ContactMapFactory(initial_complex.receptor_chains, lig, trajectory.template, mth=scm.execute)      #where from comes rec ch name?
-    cmaps = cmf.mk_cmap(trajectory.coordinates, 6.5)
-    for n, cmap in enumerate(cmaps):
-        with open('res_btest/cm_%s_%i.txt' % (lig, n), 'w') as f:
-            cmap.save_txt(f)
-        cmap.save_png('res_btest/cm_%s_%i' % (lig, n))
-    #~ import pdb; pdb.set_trace()
-
-#~ def fnc(v, n):
-    #~ for j, i in enumerate(v): cmd.pseudoatom(n + str(j), pos=i)
-
-#~ from string import ascii_letters
-
-#~ fnc(map(tuple, thx), 'a')
+j = Job(receptor='2gb1', ligand=[['MICHAL'], ['LAHCIM']], mc_cycles=2,  mc_steps=1, replicas=2, dbg=True)
+j.initial_complex = ProteinComplex(j.config)
+j.mk_cmaps(tr, c, flt, 4.5)
