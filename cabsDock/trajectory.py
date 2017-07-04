b3c1cdd5b4506b6262e84954f3c7d4c79ef8d345
mcmny <maciej.ciemny@gmail.com>
2017-07-04 17:14:11 +0200

    re-push

--- cabsDock/trajectory.py
+++ cabsDock/trajectory.py
@@ -56,23 +56,25 @@ class Header:
                 h.energy = np.concatenate([self.energy, other.energy])
         return h
 
-    def get_energy(self, number_of_peptides):
-        #number_of_peptides fixes energy calculations
-        """ Returns the total energy of the system. """
-        if not number_of_peptides:
-            print("Unknown number of peptides. Assuming 1.")
-            num_pept = 1
-        else:
-            num_pept = number_of_peptides
-        int_submtrx_size = self.energy.shape[0]-num_pept
-        int_enrg = np.sum(self.energy[:int_submtrx_size,-num_pept:])
-        # print('whole')
-        # print(self.energy)
-        # print('int')
-        # print(int_submtrx_size)
-        # print (self.energy[:int_submtrx_size,-num_pept:])
-        # print int_enrg
-        return int_enrg
+    def get_energy(self, mode='interaction', number_of_peptides=None):
+        if mode == 'interaction':
+            #number_of_peptides fixes energy calculations
+            if number_of_peptides is None:
+                print("Unknown number of peptides. Assuming 1.")
+                num_pept = 1
+            else:
+                num_pept = number_of_peptides
+            int_submtrx_size = self.energy.shape[0]-num_pept
+            int_enrg = np.sum(self.energy[:int_submtrx_size,-num_pept:])
+            # print('whole')
+            # print(self.energy)
+            # print('int')
+            # print(int_submtrx_size)
+            # print (self.energy[:int_submtrx_size,-num_pept:])
+            # print int_enrg
+            return int_enrg
+        elif mode == 'total':
+            return np.sum(np.tril(self.energy))
 
 
 class Trajectory(object):
