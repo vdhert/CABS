9d5473d465bef9c2d0a492b4871b5eebf247ef06
mkurc <mkurc@chem.uw.edu.pl>
2017-07-05 12:39:00 +0200

    funkcja __main__() uruchamia run_job(), w ktorym tworzy sie namespace args populowany opcjami z lini komend

--- cabsDock/__main__.py
+++ cabsDock/__main__.py
@@ -1,8 +1,13 @@
-import argparse
+from optparser import ParserFactory
+from pkg_resources import resource_filename
 
 
 def run_job():
-    print 'Zonk'
+    args = ParserFactory(
+        filecsv=resource_filename('cabsDock', 'data/data3.dat'),
+        fields=(1, 2, 3, 4, 5),
+        sep=','
+    ).parser.parse_args()
 
 if __name__ == '__main__':
     run_job()
