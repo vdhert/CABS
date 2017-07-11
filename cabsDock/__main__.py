2f75a7ba3f3223f9768ee63691bdf670fa447dc8
Mateusz Kurcinski <mkurc@chem.uw.edu.pl>
2017-07-11 16:15:00 +0200

    temp

--- cabsDock/__main__.py
+++ cabsDock/__main__.py
@@ -1,5 +1,15 @@
-from optparser import ParserFactory
+from cabsDock.optparser import ParserFactory
 from pkg_resources import resource_filename
+from ConfigParser import ConfigParser
+from StringIO import StringIO
+from cabsDock.job import Job
+
+
+class Config(dict):
+    def __init__(self, cl_dict, cfg_dict={}):
+        config = cfg_dict
+
+        dict.__init__(self, **config)
 
 
 def run_job():
@@ -8,6 +18,26 @@ def run_job():
         fields=(1, 2, 3, 4, 5),
         sep=','
     ).parser.parse_args()
+
+    if args.config:
+        with open(args.config) as f:
+            f = StringIO('[config]\n' + f.read())
+            config_parser = ConfigParser()
+            config_parser.readfp(f)
+            cfg_args = dict(config_parser.items('config'))
+    else:
+        cfg_args = {}
+
+    config = Config(
+        cl_dict=vars(args),
+        cfg_dict=cfg_args
+    )
+
+    for k in config:
+        print k + ':', config[k]
+    # job = Job(**config)
+    # job.run_job()
+
 
 if __name__ == '__main__':
     run_job()
