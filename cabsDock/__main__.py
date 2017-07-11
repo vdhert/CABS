a3e1cdb4c9c54b88fe21fc7848af0a3c9a234ad3
Mateusz Kurcinski <mkurc@chem.uw.edu.pl>
2017-07-11 16:15:00 +0200

    P1-36, P1-13 Przepisalem main, dodalem ConfigParser, dodalem obsluge wiezow

--- cabsDock/__main__.py
+++ cabsDock/__main__.py
@@ -1,13 +1,58 @@
-from optparser import ParserFactory
+from cabsDock.optparser import ParserFactory
 from pkg_resources import resource_filename
+from ConfigParser import SafeConfigParser
+from StringIO import StringIO
+from sys import argv
+
+
+class Config(dict):
+    def __init__(self, config):
+        """
+        Smart dictionary that reads argparse.Namespace returned by argparse.parse_args().
+        also checks / updates some of the parsed options
+        :param config: argparse.Namespace
+        """
+
+        # config.peptide + config.add_peptide -> config.ligand
+        config.ligand = []
+        if config.peptide:
+            config.ligand.extend([[p, 'random', 'random'] for p in config.peptide])
+        if config.add_peptide:
+            config.ligand.extend([p for p in config.add_peptide if p])
+
+        dict.__init__(self, vars(config))
+
+    def __repr__(self):
+        return '\n'.join([k + ': ' + str(v) for k, v in sorted(self.items())])
 
 
 def run_job():
-    args = ParserFactory(
+    parser = ParserFactory(
         filecsv=resource_filename('cabsDock', 'data/data3.dat'),
         fields=(1, 2, 3, 4, 5),
         sep=','
-    ).parser.parse_args()
+    ).parser
+
+    args = parser.parse_args()
+
+    cfg_args = []
+    if args.config:
+        with open(args.config) as f:
+            f = StringIO('[config]\n' + f.read())
+            config_parser = SafeConfigParser(allow_no_value=True)
+            # config_parser.optionxform = lambda x: x
+            config_parser.readfp(f)
+            for k, v in config_parser.items('config'):
+                cfg_args.append('--' + k)
+                cfg_args.extend(v.split())
+
+    args = parser.parse_args(cfg_args + argv[1:])
+    config = Config(args)
+
+    from cabsDock.job import Job
+    job = Job(**config)
+
+    # TODO insert here calls to Job's procedures.
 
 if __name__ == '__main__':
     run_job()
--- cabsDock/job.py
+++ cabsDock/job.py
@@ -27,90 +27,6 @@ from filter import Filter
 __all__ = ['Job']
 
 
-class Config(dict):
-    """
-    Smart dictionary that can append items with 'ligand' key instead of overwriting them.
-    TODO: universal list of associative options assigned to specific keywords: ligand, restraints etc.
-    """
-    def __init__(self, config):
-        dict.__init__(self, config)
-
-    def read_file(self, filename):
-        """Config file parser"""
-        patt = re.compile('(\w+)\s+=\s+([\w:.-]+)')
-        patt_start = re.compile('start_(\w+)', re.IGNORECASE)
-        patt_stop = re.compile('stop', re.IGNORECASE)
-        patt_lig = re.compile('ligand\s+=\s+(.*)', re.IGNORECASE)
-        with open(filename) as f:
-            lines = iter([re.sub('\s+', ' ', line.partition('#')[0]) for line in f.readlines()])
-
-        for line in lines:
-            match = re.match(patt_start, line)
-            if match:
-                key = match.group(1).lower()
-                self[key] = []
-                try:
-                    line = next(lines)
-                    while not re.match(patt_stop, line):
-                        self[key].append(line.strip())
-                        line = next(lines)
-                except StopIteration:
-                    pass
-            else:
-                match = re.match(patt_lig, line)
-                if match:
-                    lig = tuple(w.strip() for w in match.group(1).split(','))
-                    if 'ligand' not in self:
-                        self['ligand'] = []
-                    self['ligand'].append(lig)
-                else:
-                    match = re.match(patt, line)
-                    if match:
-                        key = match.group(1).lower()
-                        if '.' in match.group(2):
-                            try:
-                                val = float(match.group(2))
-                            except ValueError:
-                                val = match.group(2)
-                        else:
-                            try:
-                                val = int(match.group(2))
-                            except ValueError:
-                                if re.match('\A(yes|y|true|t)\Z', match.group(2), re.IGNORECASE):
-                                    val = True
-                                elif re.match('\A(no|n|false|f)\Z', match.group(2), re.IGNORECASE):
-                                    val = False
-                                else:
-                                    val = match.group(2)
-                        self[key] = val
-
-    def add_config(self, config):
-        """Parser for job parameters"""
-        if 'ligand' in config:
-            if 'ligand' not in self:
-                self['ligand'] = []
-            val = config['ligand']
-            if type(val) is list:
-                self['ligand'].extend(val)
-            elif type(val) is tuple:
-                self['ligand'].append(val)
-            elif type(val) is str:
-                self['ligand'].append((val,))
-            del config['ligand']
-            # checks the input peptide sequence for non-standard amino acids.
-            [check_peptide_sequence(peptide[0]) for peptide in self['ligand']]
-        self.update(config)
-        return self
-
-    def fix_ligands(self):
-        """Function that makes sure each ligand entry in config['ligand'] list is a 3-element tuple"""
-        if 'ligand' in self:
-            for i, ligand in enumerate(self['ligand']):
-                for j in range(len(ligand), 3):
-                    self['ligand'][i] += ('random',)
-        return self
-
-
 class Job:
     """
     Class representing single cabsDock job.
@@ -120,50 +36,8 @@ class Job:
         return '\n'.join([k + ' : ' + str(v) for k, v in sorted(self.config.items())])
 
     def __init__(self, **kwargs):
-        """
-        Job can be initialized by:
-        1. receptor=[receptor_input] The only required parameter, other taken from 'defaults'.
-        2. By providing location of the config file as in 'config=[path_to_file]'.
-        3. All parameters can be overwritten by specifying parameter=[value].
-        """
-
-        defaults = {
-            'work_dir': getcwd(),
-            'replicas': 10,
-            'mc_cycles': 50,
-            'mc_steps': 50,
-            't_init': 2.0,
-            't_final': 1.0,
-            'replicas_dtemp': 0.5,
-            'initial_separation': 20.0,
-            'ligand_insertion_clash': 0.5,
-            'ligand_insertion_attempts': 1000,
-            'ca_restraints_strength': 1.0,
-            'sg_restraints_strength': 1.0,
-            'receptor_restraints': (4, 5.0, 15.0),  # sequence gap, min length, max length
-            'dssp_command': 'mkdssp',
-            'fortran_compiler': ('gfortran', '-O2'),  # build (command, flags)
-            'filtering': 1000,  # number of models to filter
-            'clustering': (10, 100),  # number of clusters, iterations
-            'native_pdb': None,
-            'benchmark': False,
-            'AA_rebuild': True,
-            'contact_maps': True,
-            'reference_pdb': None,
-            'align': 'trivial'
-        }
-
-        self.config = Config(defaults)
-
-        # check if config should be read from a file
-        if 'config' in kwargs:
-            if exists(kwargs['config']):
-                self.config.read_file(kwargs['config'])
-            else:
-                raise IOError('Config file: ' + kwargs['config'] + ' does not exist!')
-
-        # update config with kwargs
-        self.config.add_config(kwargs).fix_ligands()
+        # TODO: defaults jesli job import
+        self.config = kwargs
 
         # making sure work_dir is abspath
         self.config['work_dir'] = abspath(self.config['work_dir'])
@@ -179,34 +53,53 @@ class Job:
         else:
             mkdir(work_dir)
 
+        self.initial_complex = None
+
+    def prepare_restraints(self):
+
+        # generate receptor restraints
+        receptor_restraints = Restraints(
+            self.initial_complex.receptor.generate_restraints(*self.config['receptor_restraints'])
+        )
+
+        # additional restraints
+        add_restraints = Restraints()
+
+        if self.config['ca_rest_add']:
+            add_restraints += Restraints(self.config['ca_rest_add'])
+
+        if self.config['sg_rest_add']:
+            add_restraints += Restraints(self.config['sg_rest_add'], sg=True)
+
+        if self.config['ca_rest_file']:
+            for filename in self.config['ca_rest_file']:
+                add_restraints += Restraints(filename)
+
+        if self.config['sg_rest_file']:
+            for filename in self.config['sg_rest_file']:
+                add_restraints += Restraints(filename, sg=True)
+
+        receptor_restraints += add_restraints.update_id(self.initial_complex.new_ids)
+
+        return receptor_restraints
+
+    def process_excluding(self):
+        pass
+
     def run_job(self):
         work_dir = self.config['work_dir']
         print('CABS-docking job {0}'.format(self.config['receptor']))
+
         # prepare initial complex
-        # noinspection PyAttributeOutsideInit
         print(' Building complex...')
         self.initial_complex = ProteinComplex(self.config)
         print(' ... done.')
-        # generate restraints
-        # noinspection PyAttributeOutsideInit
-        self.restraints = \
-            Restraints(self.initial_complex.receptor.generate_restraints(*self.config['receptor_restraints']))
-        add_restraints = Restraints(self.config.get('ca_restraints'))
-        add_restraints += Restraints(self.config.get('sg_restraints'), sg=True)
-        add_restraints += Restraints(self.config.get('ca_restraints_file'))
-        add_restraints += Restraints(self.config.get('ca_restraints_file'), sg=True)
-        self.restraints += add_restraints.update_id(self.initial_complex.new_ids)
 
         # run cabs
         print('CABS simulation starts.')
-        cabs_run = CabsRun(self.initial_complex, self.restraints, self.config)
+        cabs_run = CabsRun(self.initial_complex, self.prepare_restraints(), self.config)
         cabs_run.run()
-        # bar = ProgressBar(100, msg='CABS is running:')
-        # while cabs_run.is_alive():
-        #     print('isAlive')
-        #     bar.update(cabs_run.status())
-        #     sleep(5)
-        # bar.done()
+
         print('CABS simuation is DONE.')
         trajectory = cabs_run.get_trajectory()
         trajectory.template.update_ids(self.initial_complex.receptor.old_ids, pedantic=False)
@@ -231,7 +124,8 @@ class Job:
                                         align_mth=self.config['align']
                                         )
         trajectory.align_to(self.initial_complex.receptor)
-        #energy fix
+
+        # energy fix
         trajectory.number_of_peptides = len(self.initial_complex.ligand_chains)
         tra, flt_inds = Filter(trajectory, N=1000).cabs_filter()
         tra.number_of_peptides = len(self.initial_complex.ligand_chains)
@@ -253,22 +147,23 @@ class Job:
             plot_rmsd_N(    rmslst.reshape(self.config['replicas'], -1),
                             pltdir + '/RMSDn')
 
-
-        medoids, clusters_dict, clusters = Clustering(tra, 'chain ' + ','.join(self.initial_complex.ligand_chains)).cabs_clustering()
+        medoids, clusters_dict, clusters = Clustering(
+            tra, 'chain ' + ','.join(self.initial_complex.ligand_chains)
+        ).cabs_clustering()
 
         if self.config['contact_maps']:
             self.mk_cmaps(trajectory, medoids, clusters_dict, flt_inds, 4.5, pltdir)
 
-        #Saving the trajectory to PDBs:
+        # Saving the trajectory to PDBs:
         trajectory.to_pdb(mode = 'replicas', to_dir = work_dir)
-        #Saving top1000 models to PDB:
+        # Saving top1000 models to PDB:
         tra.to_pdb(mode = 'replicas', to_dir= work_dir, name='top1000' )
 
-        #Saving clusters in CA representation
+        # Saving clusters in CA representation
         for i, cluster in enumerate(clusters):
             cluster.to_pdb(mode='replicas', to_dir=work_dir, name='cluster_{0}'.format(i))
 
-        #Saving top10 models:
+        # Saving top10 models:
         if self.config['AA_rebuild']:
             # Saving top 10 models in AA representation:
             pdb_medoids = medoids.to_pdb()
@@ -276,7 +171,7 @@ class Job:
             for i, file in enumerate(pdb_medoids):
                 ca2all(file, output='model_{0}.pdb'.format(i), iterations=1, verbose=False)
         else:
-            #Saving top 10 models in CA representation:
+            # Saving top 10 models in CA representation:
             medoids.to_pdb(mode='models', to_dir=work_dir, name='model')
 
         # dictionary holding results
@@ -288,11 +183,12 @@ class Job:
         results['lowest_10k'] = sorted(results['rmsds_10k'])[0]
         results['lowest_1k'] = sorted(results['rmsds_1k'])[0]
         results['lowest_10'] = sorted(results['rmsds_10'])[0]
-        #Saving rmsd results
+
+        # Saving rmsd results
         with open(join(work_dir, 'rmsds.txt'), 'w') as outfile:
             outfile.write(str(results))
 
-        # Not returned by deafault unless self.config['benchmark'] == True.
+        # Not returned by default unless self.config['benchmark'] == True.
         if self.config['benchmark']:
             return results
 
