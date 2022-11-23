aff121846d3548f536ea3193da79100e86b867e5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 08:12:52 +0000

    Adding tarfile member sanitization to extractall()

--- CABS/cabs.py
+++ CABS/cabs.py
@@ -157,7 +157,29 @@ class CabsRun(Thread):
         )
 
         with tarfile.open(resource_filename('CABS', 'data/data1.dat')) as f:
-            f.extractall(cabs_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, cabs_dir)
 
         self.cfg = {
             'cwd': cabs_dir,
--- CABS/job.py
+++ CABS/job.py
@@ -225,7 +225,26 @@ class CABSTask(object):
                 logger.info(module_name=_name,
                             msg="Loading CABS files from %s" % files[0])
                 with tarfile.open(files[0], "r:gz") as f:
-                    f.extractall(os.path.join(self.work_dir))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(f, os.path.join(self.work_dir))
 
             else:
                 raise IOError
@@ -236,7 +255,26 @@ class CABSTask(object):
                 with tarfile.open(files_loc, "r:gz") as f:
                     logger.info(module_name=_name,
                                 msg="Loading CABS files from %s" % files_loc)
-                    f.extractall(os.path.join(self.work_dir))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(f, os.path.join(self.work_dir))
             except IOError:
                 raise
         return
